8ced6dedb508d43cb93034fb7f234c0f8900ac75
hayden1126 <haydenleung1126@gmail.com>
2025-04-23 22:14:27 -0400

    completed hw8

---
+++ hw8/KBestCounter.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class KBestCounter<T extends Comparable<? super T>> implements KBest<T>
+{
+    int k;
+    PriorityQueue<T> queue;
+
+    public KBestCounter(int k) {
+        this.k = k;
+        queue = new PriorityQueue<T>();
+    }
+
+	public void count(T x) {
+        // process the next element in the set of data. This operation must run in at worst O(log k) time.
+        queue.add(x);
+
+        if (queue.size() > k) {
+            queue.poll();
+        }
+    }
+
+	public List<T> kbest() {
+        PriorityQueue<T> tmpQueue = new PriorityQueue<T>(queue);
+        List<T> result = new ArrayList<T>();
+
+        while (!queue.isEmpty()) {
+            result.add(tmpQueue.poll());
+        }
+
+        return result;
+    }
+
+}
--- hw8/SpellChecker.java
+++ hw8/SpellChecker.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 import java.util.*;
 import java.io.File;
 
-public class SpellChecker {
+public class SpellChecker implements SpellCheckerInterface{
 
     private HashSet<String> dict;
 
